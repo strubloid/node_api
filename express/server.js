c7c4ad353ecf7f9a16b745c2eeb36aa7dec48c7c
strubloid <joserafaelmb@gmail.com>
2020-10-30 17:41:43 +0000

    another test

--- express/server.js
+++ express/server.js
@@ -7,15 +7,9 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-
-router.get('/test', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js2!</h1>');
-  res.end();
+  res.json({
+    'rafa' : 'test'
+  });
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
