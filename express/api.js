6e5ca51dfd2017cd2b55e0c1f77133f9b42f3ec7
strubloid <joserafaelmb@gmail.com>
2020-10-30 19:33:02 +0000

    test

---
+++ express/api.js
@@ -0,0 +1,25 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.json({
+    'rafa' : 'API homepage'
+  });
+});
+
+router.get('/test', (req, res) => {
+  res.json({
+    'rafa' : 'API/TEST '
+  });
+});
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/api', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
