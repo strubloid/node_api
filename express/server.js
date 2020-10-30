b226eccbd974668be48c171fc9b8edbe2634d222
strubloid <strubloid@gmail.com>
2020-10-30 14:17:21 +0000

    tring to get some values

--- express/server.js
+++ express/server.js
@@ -6,13 +6,18 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+app.get('/test', (req, res) => {
+  res.json({
+    'test' : true
+  });
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.post('/test2', (req, res) => {
+  res.json({
+    'test' : true
+  });
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
